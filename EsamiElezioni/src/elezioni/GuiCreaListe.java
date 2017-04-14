993741877372dfeb038588a464a8f495c72dda89
MUNARID <MUNARID@PORT-MUNARI2.spal.lan>
2017-04-14 16:20:44 +0200

    Commit 2

--- EsamiElezioni/src/elezioni/GuiCreaListe.java
+++ EsamiElezioni/src/elezioni/GuiCreaListe.java
@@ -10,172 +10,180 @@ import javax.swing.*;
 
 public class GuiCreaListe extends JFrame {
 
-  public JList cittadini;
-  public JList liste;
-  public JButton addCapoLista;
-  public JButton addCandidato;
-  public JTextField nome;
-  public JTextArea motto;
-  public JButton registraLista;
-  public GuiCreaListe(Elezione elezione) {
-    super("Elezioni - Creazione Liste");
-    Container cp = this.getContentPane();
-    GridBagLayout gbl = new GridBagLayout();
-    cp.setLayout(gbl);
+	public JList cittadini;
+	public JList liste;
+	public JButton addCapoLista;
+	public JButton addCandidato;
+	public JTextField nome;
+	public JTextArea motto;
+	public JButton registraLista;
 
-    GridBagConstraints c = new GridBagConstraints();
+	public GuiCreaListe(Elezione elezione) {
+		super("Elezioni - Creazione Liste");
+		Container cp = this.getContentPane();
+		GridBagLayout gbl = new GridBagLayout();
+		cp.setLayout(gbl);
 
-    // lista cittadini
-    c.gridx = 0;
-    c.gridy = 0;
-    c.gridheight = 2;
-    c.weighty = 1;
-    c.weightx = 1;
-    c.fill = GridBagConstraints.BOTH;
-    cittadini = new JList();
-    JScrollPane sp = new JScrollPane(cittadini);
-    gbl.setConstraints(sp, c);
-    cp.add(sp);
+		GridBagConstraints c = new GridBagConstraints();
 
-    // lista liste
-    c.gridx = 2;
-    c.gridy = 0;
-    c.gridheight = 2;
-    c.weighty = 1;
-    c.weightx = 1;
-    c.fill = GridBagConstraints.BOTH;
-    liste = new JList();
-    sp = new JScrollPane(liste);
-    gbl.setConstraints(sp, c);
-    cp.add(sp);
+		// lista cittadini
+		c.gridx = 0;
+		c.gridy = 0;
+		c.gridheight = 2;
+		c.weighty = 1;
+		c.weightx = 1;
+		c.fill = GridBagConstraints.BOTH;
+		cittadini = new JList();
+		JScrollPane sp = new JScrollPane(cittadini);
+		gbl.setConstraints(sp, c);
+		cp.add(sp);
 
-    // pulsante aggiungi capo lista
-    c.gridx = 1;
-    c.gridy = 0;
-    c.gridheight = 1;
-    c.weighty = 1;
-    c.weightx = 1;
-    c.fill = GridBagConstraints.NONE;
-    addCapoLista = new JButton("Capo Lista");
-    gbl.setConstraints(addCapoLista, c);
-    cp.add(addCapoLista);
+		// lista liste
+		c.gridx = 2;
+		c.gridy = 0;
+		c.gridheight = 2;
+		c.weighty = 1;
+		c.weightx = 1;
+		c.fill = GridBagConstraints.BOTH;
+		liste = new JList();
+		sp = new JScrollPane(liste);
+		gbl.setConstraints(sp, c);
+		cp.add(sp);
 
-    // pulsante aggiungi candidato
-    c.gridx = 1;
-    c.gridy = 1;
-    c.gridheight = 1;
-    c.weighty = 1;
-    c.weightx = 1;
-    c.fill = GridBagConstraints.NONE;
-    addCandidato = new JButton("Candidato");
-    gbl.setConstraints(addCandidato, c);
-    cp.add(addCandidato);
+		// pulsante aggiungi capo lista
+		c.gridx = 1;
+		c.gridy = 0;
+		c.gridheight = 1;
+		c.weighty = 1;
+		c.weightx = 1;
+		c.fill = GridBagConstraints.NONE;
+		addCapoLista = new JButton("Capo Lista");
+		gbl.setConstraints(addCapoLista, c);
+		cp.add(addCapoLista);
 
-    // label nome
-    c.gridx = 0;
-    c.gridy = 2;
-    c.gridheight = 1;
-    c.weighty = 0;
-    c.weightx = 0;
-    c.fill = GridBagConstraints.NONE;
-    JLabel lbl = new JLabel("Nome:");
-    gbl.setConstraints(lbl, c);
-    cp.add(lbl);
+		// pulsante aggiungi candidato
+		c.gridx = 1;
+		c.gridy = 1;
+		c.gridheight = 1;
+		c.weighty = 1;
+		c.weightx = 1;
+		c.fill = GridBagConstraints.NONE;
+		addCandidato = new JButton("Candidato");
+		gbl.setConstraints(addCandidato, c);
+		cp.add(addCandidato);
 
-    // label motto
-    c.gridx = 1;
-    c.gridy = 2;
-    c.gridheight = 1;
-    c.weighty = 0;
-    c.weightx = 0;
-    c.fill = GridBagConstraints.NONE;
-    lbl = new JLabel("Motto:");
-    gbl.setConstraints(lbl, c);
-    cp.add(lbl);
+		// label nome
+		c.gridx = 0;
+		c.gridy = 2;
+		c.gridheight = 1;
+		c.weighty = 0;
+		c.weightx = 0;
+		c.fill = GridBagConstraints.NONE;
+		JLabel lbl = new JLabel("Nome:");
+		gbl.setConstraints(lbl, c);
+		cp.add(lbl);
 
-    // text nome
-    c.gridx = 0;
-    c.gridy = 3;
-    c.weighty = 0;
-    c.weightx = 0;
-    c.fill = GridBagConstraints.HORIZONTAL;
-    nome = new JTextField();
-    gbl.setConstraints(nome, c);
-    cp.add(nome);
+		// label motto
+		c.gridx = 1;
+		c.gridy = 2;
+		c.gridheight = 1;
+		c.weighty = 0;
+		c.weightx = 0;
+		c.fill = GridBagConstraints.NONE;
+		lbl = new JLabel("Motto:");
+		gbl.setConstraints(lbl, c);
+		cp.add(lbl);
 
-    // pulsante registra lista
-    c.gridx = 0;
-    c.gridy = 4;
-    c.weighty = 1;
-    c.weightx = 0;
-    c.fill = GridBagConstraints.NONE;
-    c.anchor = GridBagConstraints.NORTH;
-    registraLista = new JButton("Registra");
-    gbl.setConstraints(registraLista, c);
-    cp.add(registraLista);
+		// text nome
+		c.gridx = 0;
+		c.gridy = 3;
+		c.weighty = 0;
+		c.weightx = 0;
+		c.fill = GridBagConstraints.HORIZONTAL;
+		nome = new JTextField();
+		gbl.setConstraints(nome, c);
+		cp.add(nome);
 
-    // text motto
-    c.gridx = 1;
-    c.gridy = 3;
-    c.weighty = 1;
-    c.weightx = 1;
-    c.gridheight = 2;
-    c.gridwidth = 2;
-    c.fill = GridBagConstraints.BOTH;
-    motto = new JTextArea();
-    sp = new JScrollPane(motto);
-    gbl.setConstraints(sp, c);
-    cp.add(sp);
+		// pulsante registra lista
+		c.gridx = 0;
+		c.gridy = 4;
+		c.weighty = 1;
+		c.weightx = 0;
+		c.fill = GridBagConstraints.NONE;
+		c.anchor = GridBagConstraints.NORTH;
+		registraLista = new JButton("Registra");
+		gbl.setConstraints(registraLista, c);
+		cp.add(registraLista);
 
-    setSize(500, 300);
+		// text motto
+		c.gridx = 1;
+		c.gridy = 3;
+		c.weighty = 1;
+		c.weightx = 1;
+		c.gridheight = 2;
+		c.gridwidth = 2;
+		c.fill = GridBagConstraints.BOTH;
+		motto = new JTextArea();
+		sp = new JScrollPane(motto);
+		gbl.setConstraints(sp, c);
+		cp.add(sp);
 
-    //--------------------- DATI ----------
-    // TODO aggiugnere codice per popolare le liste
-    
-    // il seguente codice è un esempio di come utilizzare le JList
-	cittadini.setListData(new String[] {
-				"Giovanni Bianchi", "Mario Rossi",
-				"Giuseppe Verdi", "Romano Prodi", "Silvio Berlusconi"});
-	liste.setListData(new String[] { "Ulivo", "Casa delle Liberta'" });
+		setSize(500, 300);
 
-    //--------------------- EVENTI ---------- 
-    setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-    
-    addCandidato.addActionListener(new ActionListener() {
-      public void actionPerformed(ActionEvent e) {
-        doAddCandidato();
-      }
-    });
-    addCapoLista.addActionListener(new ActionListener() {
-      public void actionPerformed(ActionEvent e) {
-        doAddCapoLista();
-      }
-    });
-    registraLista.addActionListener(new ActionListener() {
-      public void actionPerformed(ActionEvent e) {
-        doRegistraLista();
-      }
-    });
-  }
+		// --------------------- DATI ----------
+		// TODO aggiugnere codice per popolare le liste
 
-  void doAddCapoLista() {
-	  
-	  for (Cittadino c : Elezione.elettori) {
-		  if (liste.getSelectedValue().equals(c.getNome())   )
-		  
-	  }
-	  //liste.getSelectedValue()
+		// il seguente codice è un esempio di come utilizzare le JList
+		cittadini.setListData(new String[] { "Giovanni Bianchi", "Mario Rossi",
+				"Giuseppe Verdi", "Romano Prodi", "Silvio Berlusconi" });
+		liste.setListData(new String[] { "Ulivo", "Casa delle Liberta'" });
 
-	  
-    // TODO aggiungere gestione pulsante addCapoLista
-  }
+		// --------------------- EVENTI ----------
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 
-  void doAddCandidato() {
-    // TODO aggiungere gestione pulsante addCandidato
-  }
+		addCandidato.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				doAddCandidato();
+			}
+		});
+		addCapoLista.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				doAddCapoLista();
+			}
+		});
+		registraLista.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				doRegistraLista();
+			}
+		});
+	}
 
-  void doRegistraLista() {
-    // TODO aggiungere gestione pulsante registraLista
-  }
+	void doAddCapoLista() {
+
+		Lista l = (Lista) liste.getSelectedValue();
+		Cittadino c = (Cittadino) cittadini.getSelectedValue();
+		try {
+			l.assegnaCapolista(c);
+		} catch (CandidatoNonValido e) {
+			System.out.println("Errore");
+		}
+
+		// TODO aggiungere gestione pulsante addCapoLista
+	}
+
+	void doAddCandidato() {
+		Lista l = (Lista) liste.getSelectedValue();
+		Cittadino c = (Cittadino) cittadini.getSelectedValue();
+		try {
+			l.aggiungiCandidato(c);
+		} catch (CandidatoNonValido e) {
+			System.out.println("Errore");
+		}
+	}
+
+	void doRegistraLista() {
+		Lista l = (Lista) liste.getSelectedValue();
+		Elezione.
+	}
 
 }
